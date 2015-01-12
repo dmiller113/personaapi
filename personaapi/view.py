6ab3886a88ee3b4ee76efa758f5cd5c1670717ff
Daniel Miller <dmiller@concordusapps.com>
2015-01-12 02:56:59 -0800

    Going to bed

--- personaapi/view.py
+++ personaapi/view.py
@@ -1,5 +1,5 @@
 from personaapi import app, session
-from flask import g
+from flask import g, render_template
 
 
 @app.before_request
@@ -17,3 +17,18 @@ def teardown_request(exception):
 @app.route('/')
 def hallo():
     return 'hallo sir'
+
+
+@app.route(rule='/entity/<int:id>')
+def return_entity(id):
+    from personaapi.models import Entity
+    entity = g.db.query(Entity).filter_by(id=id).one()
+    col_names = [
+        'Arcana', 'Level', 'Strength', 'Magic', 'Endurance',
+        'Agility', 'Luck',
+    ]
+    return render_template('listview.html',
+                           entity=entity,
+                           list_name="Entity",
+                           columns=col_names,
+                           getattr=getattr)
